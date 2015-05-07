8b9185ef752ac8fab825d38cf14a1bb4c3dfcb3b
chuchu9293 <duyichu123@126.com>
2015-05-07 20:32:57 +0800

    123

--- GraduationProjectCollect/WebContent/js/app.js
+++ GraduationProjectCollect/WebContent/js/app.js
@@ -1,19 +1,43 @@
-angular.module('qingWebApp',[]);
+angular.module('myApp', []);
 
-angular.module('qingWebApp').controller("appCtrl",
-/*
-function($scope, $http) {
-	//$http.jsonp期望得到的响应是 angular.callbacks._0(34)
-	$http.jsonp(
-			"http://115.28.88.165/ws/qing/timeSpan"
-					+ "?callback=JSON_CALLBACK").success(
-			function(data) {
-				$scope.timeSpan = data;
-			}).error(function(data) {
-				console.log(data);
-	});*/
-function($scope, $http) {
-	
-}
+angular.module('myApp').controller(
+		"appController",
+		function($scope, $http) {
+			$scope.url = "http://www.people.com.cn/";
+			$scope.collectObj = new Object();
+			$scope.urlSubmit = function() {
+				// angular的$http.post()方法默认数据传输格式是json的
+				var urlJson = {
+					"url" : $scope.url
+				};
+				$http.post("/GraduationProjectCollect/webService/urlService",
+						urlJson).success(function(data) {
+					alert(data.title);
+					// $scope.collectObj.url=;
+					$scope.collectObj.title = data.title;
+					$scope.collectObj.content = data.content;
+					// $scope.collectObj.isPositive=isPositive;
+				}).error(function(data) {
+					console.log(data);
+				});
+			}
+
+			$scope.insertDB = function(url, title, content, isPositive) {
+				var collectJson = {
+					"url" : url,
+					"title" : title,
+					"content" : content,
+					"isPositive" : isPositive
+				};
+				$http.post(
+						"/GraduationProjectCollect/webService/collectService",
+						collectJson).success(function(data) {
+					alert(data.title);
+				}).error(function(data) {
+					console.log(data);
+				});
+
+			}
+		}
 
 );
